3e130d4ef51a3864ea0d5b81febe6f277053aff3
Brian Douglas Smith <smith.b@salesforce.com>
2018-03-06 15:08:09 -0800

    disable logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { RedocStandalone } from 'redoc';
 
@@ -7,10 +7,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      {/*
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        */}
         <RedocStandalone specUrl="https://gist.githubusercontent.com/bds/eada022fa12af59459d6ebeb9a54beba/raw/af4b2f2cb3822b7774d04fb0605ce8bdcc0e43c5/swagger.json" />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.

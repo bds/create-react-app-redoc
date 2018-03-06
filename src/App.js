db803a27a7fbe2f40a91177e4b3c2f53cc974e7e
Brian Douglas Smith <smith.b@salesforce.com>
2018-03-06 14:58:45 -0800

    update specUrl

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <RedocStandalone specUrl="http://rebilly.github.io/ReDoc/swagger.yaml" />
+        <RedocStandalone specUrl="https://gist.githubusercontent.com/bds/eada022fa12af59459d6ebeb9a54beba/raw/af4b2f2cb3822b7774d04fb0605ce8bdcc0e43c5/swagger.json" />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
